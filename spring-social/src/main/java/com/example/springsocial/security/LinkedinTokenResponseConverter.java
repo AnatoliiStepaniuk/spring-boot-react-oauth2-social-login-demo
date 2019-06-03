b6b5aca5d9228f97070b66b83ea0cee79ceeec81
anatolii.stepaniuk <anatolii.stepaniuk@gmail.com>
2019-06-03 21:29:30 +0300

    Fix for Github Integration

--- spring-social/src/main/java/com/example/springsocial/security/LinkedinTokenResponseConverter.java
+++ spring-social/src/main/java/com/example/springsocial/security/LinkedinTokenResponseConverter.java
@@ -6,6 +6,7 @@ import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenRespon
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class LinkedinTokenResponseConverter
         implements Converter<Map<String, String>, OAuth2AccessTokenResponse> {
@@ -13,13 +14,13 @@ public class LinkedinTokenResponseConverter
     @Override
     public OAuth2AccessTokenResponse convert(Map<String, String> tokenResponseParameters) {
         String accessToken = tokenResponseParameters.get(OAuth2ParameterNames.ACCESS_TOKEN);
-        long expiresIn = Long.valueOf(tokenResponseParameters.get(OAuth2ParameterNames.EXPIRES_IN));
 
         OAuth2AccessToken.TokenType accessTokenType = OAuth2AccessToken.TokenType.BEARER;
 
-        return OAuth2AccessTokenResponse.withToken(accessToken)
-                .tokenType(accessTokenType)
-                .expiresIn(expiresIn)
-                .build();
+        OAuth2AccessTokenResponse.Builder builder = OAuth2AccessTokenResponse.withToken(accessToken)
+                .tokenType(accessTokenType);
+        Optional.ofNullable(tokenResponseParameters.get(OAuth2ParameterNames.EXPIRES_IN))
+                .ifPresent(expiresIn -> builder.expiresIn(Long.valueOf(expiresIn)));
+        return builder.build();
     }
 }
